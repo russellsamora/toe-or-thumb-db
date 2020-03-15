28add42fa5942cb1cc783a25cf57867f8cc5d020
russellgoldenberg <russellgoldenberg@gmail.com>
2020-03-15 17:29:08 -0400

    worker

--- aggregate.js
+++ aggregate.js
@@ -2,14 +2,13 @@ const request = require("request");
 const d3 = require("d3");
 const dataS3 = require("data-s3");
 
-const accessKeyId = process.env.AWS_KEY;
-const secretAccessKey = process.env.AWS_SECRET;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.AWS_REGION;
 const bucket = process.env.AWS_BUCKET;
 
-const url = "https://toe-or-thumb-db.herokuapp.com/results";
-
 function getDB() {
+  const url = "https://toe-or-thumb-db.herokuapp.com/results";
   return new Promise((resolve, reject) => {
     request(url, (err, response, body) => {
       if (response && response.statusCode === 200) resolve(body);
@@ -18,10 +17,28 @@ function getDB() {
   });
 }
 
+function clean(raw) {
+  const data = JSON.parse(raw);
+  const answers = d3
+    .nest()
+    .key(d => d.question)
+    .rollup(v => {
+      const r = v.filter(d => d.answer).length;
+      const w = v.length - r;
+      return { r, w };
+    })
+    .entries(data.filter(d => typeof d.question === "number"));
+  return {
+    updated: new Date().toISOString(),
+    answers
+  };
+}
+
 async function init() {
   try {
     dataS3.init({ accessKeyId, secretAccessKey, region });
-    const data = await getDB();
+    const raw = await getDB();
+    const data = clean(raw);
     const path = "misc/toe-or-thumb";
     const file = "data.json";
     await dataS3.upload({ bucket, path, file, data });
